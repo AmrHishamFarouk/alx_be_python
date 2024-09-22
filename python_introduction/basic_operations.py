84e4658f0e3469d61bc7293fbef60ff12b4f5959
AmrHishamFarouk <hishamamr121@gmail.com>
2024-09-22 23:23:23 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,14 +1,14 @@
-num1= int(input("please enter your first number:"))
-num2= int(input("please enter your second number:"))
+number1= int(input("please enter your first number:"))
+number2= int(input("please enter your second number:"))
 
-addition = num1 + num2
-difference = num1 - num2
-product = num1 * num2
+addition = number1 + number2
+difference = number1 - number2
+product = number1 * number2
 
 
-print(" Addition of ",num1,'and ',num2,'is ',addition)
-print(" difference of ",num1,'and ',num2,'is ',difference)
-print(" the product of ",num1,'and ',num2,'is ',product)
+print(" Addition of ",number1,'and ',number2,'is ',addition)
+print(" difference of ",number1,'and ',number2,'is ',difference)
+print(" the product of ",number1,'and ',number2,'is ',product)
 
 
 # Addition of 10 and 5 is 15
