320fa25b85f7d29a9b3333bea9b9854ffb0ec4fc
AmrHishamFarouk <hishamamr121@gmail.com>
2024-09-22 23:27:23 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -6,8 +6,6 @@ difference = number1 - number2
 product = number1 * number2
 
 
-print(" Addition of ",number1,'and ',number2,'is ',addition)
-print(" Subtraction of ",number1,'and ',number2,'is ',difference)
-print(" Multiplication of ",number1,'and ',number2,'is ',product)
-
-
+print(" Addition of",number1,'and ',number2,'is',addition)
+print(" Subtraction of",number1,'and ',number2,'is',difference)
+print(" Multiplication of",number1,'and ',number2,'is',product)
