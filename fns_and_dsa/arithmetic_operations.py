848b0bf9d33bcefb7eb93203b13f2361ef0646fa
AmrHishamFarouk <hishamamr121@gmail.com>
2024-09-29 19:53:00 +0300

    third fix upload

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,17 +10,5 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 print("Cannot divide by zero.")
-            else:
+            elif num2 != 0:
                 return num1/num2
-
-
-
-
-
-
-
-
-
-
-
-                arithmetic_operations.py doesn't contain: ["num1, num2, operation"] / /tmp/correction/6427561300380239850411766792785119938742_5/100741/919068/fns_and_dsa/arithmetic_operations.py doesn't contain def\s*perform_operation\(num1,\s*num2,\s*operation\):
